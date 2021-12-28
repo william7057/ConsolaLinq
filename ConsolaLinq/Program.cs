2055fe9acd9f4d8ee32806d7a249a5cf98601989
william <factzure@hotmail.com>
2021-12-28 01:54:48 -0500

    Agregar archivos de proyecto.

---
+++ ConsolaLinq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolaLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numeros = { 5, 6, 8, 9, 2, 1, 3, 6, 9 };
+
+            var Mayor = (from x in Numeros
+                        select x).Max() ;
+
+            Console.WriteLine("Mayor es : {0}", Mayor);
+            Console.ReadLine();
+        }
+    }
+}
